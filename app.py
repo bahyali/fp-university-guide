62ca6d5d3335fe4b6306895100afd189b1c308c2
Bahy Ali <bahy.ali@icloud.com>
2021-05-10 23:57:40 +0200

    add login manager

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
+import os
 from flask import Flask, render_template, url_for
 from markupsafe import escape
 from flask_assets import Environment, Bundle
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+from flask_login import LoginManager
+
+load_dotenv()  # take environment variables from .env.
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.sqlite"
+# Database
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
+
+# Authenticator
+# login_manager = LoginManager()
+# login_manager.init_app(app)
 
 # Static assets
 assets = Environment(app)
 assets.url = app.static_url_path
-# assets.debug = True
 
 # Website
 website_scss = Bundle('sass/base.scss', depends='sass/**/*.scss', filters='scss', output='css/styles.css')
@@ -27,14 +36,26 @@ VIEWS_DIR = 'views'
 STYLEGUIDE_DIR = 'views/style_guide'
 
 
+# Routes
+
 @app.route('/')
 def hello_world():
     return render_template('%s/hello.html' % VIEWS_DIR)
 
 
+@app.route('/login')
+def login():
+    return render_template('%s/hello.html' % VIEWS_DIR)
+
+
+@app.route('/signup')
+def signup():
+    return render_template('%s/hello.html' % VIEWS_DIR)
+
+
 @app.route('/style_guide')
 def style_guide():
-    return render_template('%s/cards.html' % STYLEGUIDE_DIR)
+    return render_template('%s/index.html' % STYLEGUIDE_DIR)
 
 
 @app.route('/style_guide/<path:subpath>')
@@ -43,13 +64,21 @@ def style_guide_subpath(subpath):
     return render_template('%s/%s.html' % (STYLEGUIDE_DIR, escape(subpath)))
 
 
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return 'User1 %s' % escape(username)
-
-
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
     # show the post with the given id, the id is an integer
     return 'Post %d' % post_id
+
+
+@app.route('/university/<int:uni_id>')
+def show_university(uni_id):
+    # show the post with the given id, the id is an integer
+    return 'University %d' % uni_id
+
+
+@app.route('/program/<int:program_id>')
+def show_program(program_id):
+    # show the post with the given id, the id is an integer
+    return 'Program %d' % program_id
+
+
